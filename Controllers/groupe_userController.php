ed8112c37192c2468bf36511166e406deca40256
Bouanani Hanane <94290348+hananebouanani16@users.noreply.github.com>
2023-10-19 18:34:54 +0100

    Add files via upload

---
+++ Controllers/groupe_userController.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Groupe;
+use App\Models\Module;
+use App\Models\Objet;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class groupe_userController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $groups = Groupe::all();
+        $elem['role'] = Module::all();
+
+
+        $elem['perm'] = Objet::all();
+        $datas['admin'] = Auth::user()->type_utilisateur;
+        $datas['c'] = DB::table('users')
+            ->join('personnels', 'personnels.IDPersonnels', '=', 'users.idperso')
+            ->join('directions', 'directions.ID_DR5', '=', 'personnels.id_direction')
+            ->select('personnels.Nom_Personnels', 'users.id', 'users.name', 'users.tel', 'users.type_utilisateur', 'users.role', 'users.created_at', 'users.email', 'users.status', 'directions.Intitule_direction')
+
+            ->get();
+        $elem['user'] = NULL;
+        if ($datas['admin'] == '-1') {
+            $elem['user'] = $datas['c'];
+        } else if ($datas['admin'] == '0') {
+
+            $perso = Auth::user()->idperso;
+
+
+
+            $dat = DB::table('users')
+                ->join('personnels', 'personnels.IDPersonnels', '=', 'users.idperso')
+                ->join('directions', 'directions.ID_DR5', '=', 'personnels.id_direction')
+                ->where('users.idperso', '=', $perso)
+                ->select('directions.ID_DR5')
+                ->first();
+
+
+
+
+
+
+            $elem['user'] = DB::table('users')
+                ->join('personnels', 'personnels.IDPersonnels', '=', 'users.idperso')
+                ->join('directions', 'directions.ID_DR5', '=', 'personnels.id_direction')
+                ->select('users.id', 'users.name', 'users.tel', 'users.type_utilisateur', 'users.role', 'users.created_at', 'users.email', 'users.status', 'directions.num_parent', 'directions.Intitule_direction', 'personnels.Nom_Personnels', 'directions.ID_DR5')
+                ->where('directions.num_parent', '=', $dat->ID_DR5)
+                ->orWhere('personnels.id_direction', '=', $dat->ID_DR5)
+
+                ->get();
+        }
+
+        return view('users.groupeUsers', ['groups' => $groups, 'elem' => $elem]);
+    }
+
+
+    public function show(Groupe $group)
+    {
+        // Vérifier si le groupe a été trouvé
+
+        $groupes['elem'] = DB::table('groupes')
+            ->select('intitule_groupe', 'id_groupe')
+            ->where('id_groupe', '=', $group->id_groupe)
+            ->first();
+        $groupes['user'] = DB::table('group_users')
+            ->join('users', 'group_users.user_id', '=', 'users.id')
+            ->select('users.name', 'users.id')
+            ->where('group_users.groupe_id_groupe', '=', $groupes['elem']->id_groupe)
+            ->get();
+        $groupes['nbr'] = DB::table('group_users')
+            ->join('users', 'group_users.user_id', '=', 'users.id')
+            ->select('users.name', 'users.id')
+            ->where('group_users.groupe_id_groupe', '=', $groupes['elem']->id_groupe)
+            ->count();
+
+        $groupes['role'] = $modules = DB::table('groupe_modules')
+            ->join('groupes', 'groupe_modules.groupe_id_groupe', '=', 'groupes.id_groupe')
+            ->join('modules', 'modules.id', '=', 'groupe_modules.module_id')
+            ->select('modules.nom_module')
+            ->where('groupes.id_groupe', '=', $group->id_groupe)
+            ->get();
+        $groupes['perm'] = $modules = DB::table('groupe_objects')
+            ->join('groupes', 'groupe_objects.groupe_id_groupe', '=', 'groupes.id_groupe')
+            ->join('objets', 'objets.NUM_OBJECT', '=', 'groupe_objects.objet_id')
+            ->select('objets.intitule_object')
+            ->where('groupes.id_groupe', '=', $group->id_groupe)
+            ->get();
+
+        $groups = Groupe::all();
+
+
+        return view('users.groupeUsers', ['groupes' => $groupes, 'groups' => $groups]);
+    }
+
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $groupe = new Groupe();
+        $groupe->intitule_groupe = $request->input('intitule');
+        $groupe->intitule_groupe = $request->input('intitule');
+        $groupe->id_direction = $request->input('type_dr');
+
+
+        $groupe->save();
+        $group = $request->input('roles');
+
+        $groupe->role()->attach($group);
+        $perm = $request->input('permissions');
+        $groupe->perm()->attach($perm);
+        $user = $request->input('user');
+        $groupe->users()->attach($user);
+
+        DB::commit();
+
+        return redirect('groupe');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $group = Groupe::findOrFail($id);
+        $group->{$request->input('field')} = $request->input('value');
+        if ($group->save()) {
+            return response()->json(['success' => true]);
+        } else {
+            return response()->json(['success' => false]);
+        }
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ Controllers/roleController.php
@@ -0,0 +1,227 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Module;
+use App\Models\Objet;
+use App\Models\Module_objet;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class roleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+
+    public function index()
+    {
+        //$roles = Module::all();
+        $subquery = DB::table('users_modules')
+            ->select(DB::raw('MIN(module_id) as module_id'))
+            ->groupBy('module_id');
+
+
+
+
+        $roles = DB::table('modules')
+            ->leftJoin('users_modules', 'modules.id', '=', 'users_modules.module_id')
+            ->leftJoin('users', 'users_modules.user_id', '=', 'users.id')
+            ->select('modules.nom_module', 'modules.id', DB::raw('COUNT(users.id) as user_count'))
+            ->groupBy('modules.id', 'modules.nom_module')
+            ->whereNull('modules.deleted_at')
+            ->get();
+        $rolesChunks = $roles->chunk(3);
+        $permissions['o'] = Objet::all();
+        $permissions['m'] = Objet::all();
+        $perso = Auth::user()->idperso;
+        $dat = DB::table('users')
+            ->join('personnels', 'personnels.IDPersonnels', '=', 'users.idperso')
+            ->join('directions', 'directions.ID_DR5', '=', 'personnels.id_direction')
+            ->where('users.idperso', '=', $perso)
+            ->select('directions.ID_DR5')
+            ->first();
+        $permissions['p'] = DB::table('users')
+            ->join('personnels', 'personnels.IDPersonnels', '=', 'users.idperso')
+            ->join('directions', 'directions.ID_DR5', 'personnels.id_direction')
+            ->select('*')
+            ->where('personnels.id_direction', '=', $dat->ID_DR5)
+            ->Orwhere('directions.num_parent', '=', $dat->ID_DR5)
+
+            ->get();
+        foreach ($permissions['m']  as $permission) {
+            // récupère les modules qui ont l'objet avec NUM_OBJECT correspondant à la permission
+            $modules = DB::table('module_objets')
+                ->join('objets', 'objets.id', '=', 'module_objets.objet_id')
+                ->join('modules', 'modules.id', '=', 'module_objets.module_id')
+                ->whereNull('modules.deleted_at')
+                ->select('modules.nom_module', 'objets.id')
+                ->where('objets.id', '=', $permission->id)
+                ->get();
+            $permission->modules = $modules;
+            // fait quelque chose avec les modules récupérés
+            // ...
+        }
+
+
+
+
+        return view('ROLE', ['roles' => $roles, 'permissions' => $permissions]);
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $role = new Module();
+        $role->nom_module = $request->input('rolee');
+
+        $role->intitule_objet = $request->input('rolee');
+
+
+        $role->save();
+
+        $permissions = $request->input('permissions');
+        $role->permissions()->attach($permissions);
+
+
+        DB::commit();
+        return redirect('rolee');
+    }
+    public function getUsersForRole(Request $request)
+    {
+        $permissions['o'] = Objet::all();
+        $permissions['m'] = Objet::all();
+        $perso = Auth::user()->idperso;
+
+        $permissions['p'] = DB::table('users')
+            ->join('personnels', 'personnels.IDPersonnels', '=', 'users.idperso')
+            ->select('*')
+            ->where('users.idperso', '=', $perso)
+            ->get();
+        foreach ($permissions['m']  as $permission) {
+            // récupère les modules qui ont l'objet avec NUM_OBJECT correspondant à la permission
+            $modules = DB::table('module_objets')
+                ->join('objets', 'objets.id', '=', 'module_objets.objet_id')
+                ->join('modules', 'modules.id', '=', 'module_objets.module_id')
+                ->select('modules.nom_module', 'objets.id')
+                ->where('objets.id', '=', $permission->id)
+                ->get();
+            $permission->modules = $modules;
+            // fait quelque chose avec les modules récupérés
+            // ...
+        }
+        $roles = Module::all();
+        $roleId = $request->input('role_id');
+
+        $users = DB::table('users_modules')
+            ->join('users', 'users.id', '=', 'users_modules.user_id')
+            ->select('*')
+            ->where('users_modules.module_id', '=', $roleId)
+            ->get();
+        // console.log($roles);
+        return view('ROLE', ['users' => $users, 'roles' => $roles, 'permissions' => $permissions]);
+    }
+    public function add(Request $request)
+    {
+
+        $perm = new Objet();
+        $perm->intitule_object = $request->input('intitule');
+        $perm->save();
+        return redirect('rolee');
+    }
+    public function adduser(Request $request, $id)
+    {
+        $users = $request->input('multiple_selection_users');
+        //dd($users);
+        //dd($request->all());
+        if ($users !== null) {
+            // dd("hhhhhhhhh");
+            foreach ($users as $use) {
+
+                /*$module = DB::table('modules')
+        ->select('num_module')
+        ->where('modules.num_module', '=', $id)
+        ->first();
+        dd($module);*/
+                if ($id) {
+                    DB::table('users_modules')->insert([
+                        'user_id' => $use,
+                        'module_id' => $id
+                    ]);
+                }
+            }
+        }
+        return redirect('rolee');
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($num_module)
+    {
+        $module = Module::where('id', '=', $num_module)->first();
+
+        if ($module) {
+            $module->delete();
+        }
+        return redirect('rolee');
+    }
+}
